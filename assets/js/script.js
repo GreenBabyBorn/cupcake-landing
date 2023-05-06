4558b4d2ea6852e744c96f7ec2c40672febc0be0
greenbabyborn <greenbabyborn.com>
2023-05-06 18:29:36 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let header = document.querySelector(".header");
+let sticky = header.offsetTop;
+
+function stickyScroll() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function () {
+  stickyScroll();
+};
+
+const wrapper = document.querySelector(".welcome");
+const layers = document.querySelectorAll(`[class*="berry-"]`);
+
+let bg = document.querySelectorAll('[class*="berry-"]');
+for (let i = 0; i < bg.length; i++) {
+  window.addEventListener("mousemove", function (e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;
+    bg[i].style.transform = "translate(-" + x * 50 + "px, -" + y * 50 + "px)";
+  });
+}
